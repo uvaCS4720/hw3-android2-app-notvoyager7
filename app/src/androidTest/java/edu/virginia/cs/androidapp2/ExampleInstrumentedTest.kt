1c07c4d1a98f159d0a2c43253a219193094eb530
voyager7 <kenningspath@gmail.com>
2026-03-03 18:55:39 -0800

    updated the string in the ExampleInstrumentedTest to match the new package name

--- app/src/androidTest/java/edu/virginia/cs/androidapp2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/edu/virginia/cs/androidapp2/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        Assert.assertEquals("edu.nd.pmcburne.hwapp.one", appContext.packageName)
+        Assert.assertEquals("edu.virginia.cs.androidapp2", appContext.packageName)
     }
 }
