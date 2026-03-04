207a0b0c09adfaf7b00d93fd53e6fe17338b5b09
voyager7 <kenningspath@gmail.com>
2026-03-03 19:11:51 -0800

    updated theme and related files. installed google fonts dependency

--- app/src/main/java/edu/virginia/cs/androidapp2/MainActivity.kt
+++ app/src/main/java/edu/virginia/cs/androidapp2/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import edu.virginia.cs.androidapp2.ui.theme.HWStarterRepoTheme
+import edu.virginia.cs.androidapp2.ui.theme.AndroidApp2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HWStarterRepoTheme {
+            AndroidApp2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    HWStarterRepoTheme {
+    AndroidApp2Theme {
         Greeting("Android")
     }
 }
