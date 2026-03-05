81e09703d45e3248b75a47463be73c505794d8fe
voyager7 <kenningspath@gmail.com>
2026-03-05 01:26:15 -0800

    started building GameRepository

--- app/src/main/java/edu/virginia/cs/androidapp2/GameRepository.kt
+++ app/src/main/java/edu/virginia/cs/androidapp2/GameRepository.kt
@@ -1,7 +1,16 @@
 package edu.virginia.cs.androidapp2
 
-class GameRepository(
-    private val gameDao: GameDao
-) {
+import kotlinx.coroutines.flow.Flow
 
+class GameRepository(
+    private val gameDao: GameDao,
+    private val gameAPI: GameAPIService
+) {
+    fun getGames(date: Long, gender: String): Flow<List<Game>> {
+        return gameDao.getGames(date, gender)
+    }
+
+    suspend fun refreshGames(date: Long, gender: String) {
+
+    }
 }
